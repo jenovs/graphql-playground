c0ce658ec657b8cbd1c03a910427eff9bcf773b1
Viktors Jenovs <jviktors@gmail.com>
2018-03-17 16:01:31 +0200

    Add mutation to the client

---
+++ client/src/AddTodo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+import { todosListQuery } from './TodosListWithData';
+
+const AddTodo = ({ mutate }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const input = e.target.querySelector('input');
+    console.log(input);
+    mutate({
+      variables: { name: input.value },
+      refetchQueries: [{ query: todosListQuery }],
+    }).then(() => (input.value = ''));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input placeholder="New Todo" autoFocus />
+    </form>
+  );
+};
+
+const addTodoMutation = gql`
+  mutation addTodo($name: String!) {
+    addTodo(name: $name) {
+      id
+      name
+    }
+  }
+`;
+export default graphql(addTodoMutation)(AddTodo);
---
+++ client/src/TodosListWithData.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import AddTodo from './AddTodo';
+
+export const todosListQuery = gql`
+  query todosListQuery {
+    todos {
+      id
+      name
+      completed
+    }
+  }
+`;
+
+const TodosList = ({ data: { loading, error, todos } }) => {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <React.Fragment>
+      <AddTodo />
+      <ul>{todos.map(todo => <li key={todo.id}>{todo.name}</li>)}</ul>
+    </React.Fragment>
+  );
+};
+
+const TodosListWithData = graphql(todosListQuery, {
+  options: {
+    // refetch queries after set timeout
+    pollInterval: 5000,
+  },
+})(TodosList);
+
+export default TodosListWithData;
